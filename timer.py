fc7ec4e305ea9fda840b9714627fbbc2e6f3ad3e
Barry Tarlton <TARLTOB1@nationwide.com>
2017-10-05 20:28:59 -0400

    removed unused code and imports

--- timer.py
+++ timer.py
@@ -1,6 +1,4 @@
 import logging
-import time
-import display_7segment as display
 import timer_thread
 
 running_thread = None
@@ -13,36 +11,11 @@ def stopCountDown():
 
 
 
-def countDown(minutes):
-    logging.info("in Countdown:" + str(minutes))
+def countDown(timeToEnd):
+    logging.info("in Countdown:" + str(timeToEnd))
     global running_thread
     if running_thread is not None:
         running_thread.stop()
-    running_thread = timer_thread.TimerThread(minutes=minutes)
+    running_thread = timer_thread.TimerThread(timeToEnd=timeToEnd)
     running_thread.start()
 
-
-
-def timeKeeper(minutes):    
-    global running_thread
-    sec = minutes * 60
-    #build initial timer string:
-    timeStr = str(minutes) + ':' + str(sec % 60) 
-    #logging.info("Initial time String:" + str(timeStr))
-    #get current system time
-    startTimeInSeconds = time.time()
-    #get what the endtime should be
-    endTimeInSeconds = startTimeInSeconds + sec
-    global is7SegmentDisplayAvailable
-
-    while ((sec > 1) and getattr(running_thread, "do_run", True)):
-        
-        sec = endTimeInSeconds - time.time() # seconds left should be endTime - current time
-        mins = int(sec/60)
-        secondsForTimer = int(sec % 60)
-        #timeStr = str(int(sec/60)) + ':' + str(sec % 60) 
-        timeStr = '{:02d}{:02d}'.format(mins, secondsForTimer)
-        
-        #Write out to display
-        display.writeToDisplay(timeStr)
-        time.sleep(.25) # wait 1/4 a second before checking time again
